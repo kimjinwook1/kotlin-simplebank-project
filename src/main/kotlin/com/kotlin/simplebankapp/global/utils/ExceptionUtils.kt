d6a488f6baac3ac09bc26ea576f7c930d0c4e53a
kimjinwook1 <rlawlsdnr628@naver.com>
2022-11-12 16:32:33 +0900

    RuntimeException -> BusinessException 변경

--- src/main/kotlin/com/kotlin/simplebankapp/global/utils/ExceptionUtils.kt
+++ src/main/kotlin/com/kotlin/simplebankapp/global/utils/ExceptionUtils.kt
@@ -1,12 +1,13 @@
 package com.kotlin.simplebankapp.global.utils
 
+import com.kotlin.simplebankapp.global.error.BusinessException
 import com.kotlin.simplebankapp.global.error.ErrorCode
 import org.springframework.data.repository.CrudRepository
 import org.springframework.data.repository.findByIdOrNull
 
-fun memberNotFound(): Nothing = throw RuntimeException(ErrorCode.MEMBER_NOT_FOUND.message)
+fun memberNotFound(): Nothing = throw BusinessException(ErrorCode.MEMBER_NOT_FOUND)
 
-fun fail(error: ErrorCode): Nothing = throw RuntimeException(error.message)
+fun fail(error: ErrorCode): Nothing = throw BusinessException(error)
 
 fun <T, ID> CrudRepository<T, ID>.findByIdOrThrow(id: ID): T {
     return this.findByIdOrNull(id) ?: memberNotFound()
